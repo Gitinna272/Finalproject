8b1097a0600279838b3436472d8e130e5ceb80ef
Gitinna272 <fokeevainna25@gmail.com>
2024-11-01 11:39:01 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,5 @@
 # Фокеева Инна, 23-я когорта — Финальный проект. Инженер по тестированию расширенный
 
-
 URL_SERVICE = "https://e3b3d20d-778d-420d-9276-26e209c51ffd.serverhub.praktikum-services.ru/"
 ORDER_CREATE = "/api/v1/orders"
 ORDER_GET = "/api/v1/orders/track"
